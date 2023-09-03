b5ae4d1df576fafc132e23944c720f2f3f5a57e1
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-03 19:41:21 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from './../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo Ignite Shop" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
