dcd610c2301e8aba23d70907a7e35fd8af6a7c16
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-03 15:00:53 -0300

    Fundamentos do Next.js - Configurando documento HTML

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,29 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
+        {/* => Equivalente ao Root de uma aplicação React */}
         <Main />
+
+        {/* => Define em qual lugar do html o Javascript sera renderizado */}
         <NextScript />
       </body>
     </Html>
   )
 }
+
+// IMPORTANTE
+// TODA VEZ QUE ESSE ARQUIVO FOR ALTERADO É NECESSÁRIO REINICIAR O SERVIDOR NODE.
