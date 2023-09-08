1d596da85cda16bb622aacd9c1827ddc5e713de5
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-07 21:52:56 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
 import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
 
-export const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-})
+import camiseta1 from './../assets/camisetas/1.png'
+import camiseta2 from './../assets/camisetas/2.png'
+import camiseta3 from './../assets/camisetas/3.png'
+import Image from 'next/image'
 
 export default function Home() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <span>tioadksoaskda</span>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="Produto" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="Produto" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      {/* <Product>
+        <Image src={camiseta3} width={520} height={480} alt="Produto" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
+  )
 }
