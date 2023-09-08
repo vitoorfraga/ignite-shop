59730b5ac0fbc0b01b96d51d3fdb9fe20445245e
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-08 12:54:42 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Success() {
-  return <div>Success</div>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>...</ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong> , sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
