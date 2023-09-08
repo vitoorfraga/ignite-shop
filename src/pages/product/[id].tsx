305cf138d02631cd7cb6588d0fe56fab430b03bf
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-08 12:34:22 -0300

    Produto & Checkout - Botão de comprar

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,9 +4,10 @@ import {
   ProductContainer,
   ProductDetails,
 } from '@/styles/pages/product'
+import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import Stripe from 'stripe'
 
 interface ProductProps {
@@ -21,8 +22,23 @@ interface ProductProps {
 }
 
 export default function Produto({ product }: ProductProps) {
-  function handleBuyProduct() {
-    console.log(product.defaultPriceId)
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
+  async function handleBuyProduct() {
+    setIsCreatingCheckoutSession(true)
+    try {
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch {
+      alert('Falha ao direcionar ao checkout')
+      setIsCreatingCheckoutSession(false)
+      // Conectar com uma ferramenta de iobservalidade (datadog / Sentry)
+    }
   }
 
   return (
@@ -42,7 +58,9 @@ export default function Produto({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct}>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -59,8 +59,13 @@ export const ProductDetails = styled('div', {
     cursor: 'pointer',
     fontSize: '$md',
 
-    '&:hover': {
+    '&:not(:disabled):hover': {
       backgroundColor: '$green300',
+    },
+
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
     },
   },
 })
