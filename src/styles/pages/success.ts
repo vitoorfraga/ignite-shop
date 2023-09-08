5e052d65e99b59e0010ff87a82c6d8ba7f0685e7
Vitor Fraga <vitorfragaps@gmail.com>
2023-09-08 13:40:12 -0300

    Produto & Checkout - Dados da compra no sucesso

--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -43,6 +43,7 @@ export const ImageContainer = styled('div', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465da 100%)',
   borderRadius: 8,
   padding: '0.25rem',
+  display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 
